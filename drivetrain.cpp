8fb276047c95e06a9561a6f2d310ffa908b92c3c
SomewhatMay <umayeer.ahsan@hotmail.com>
2024-12-13 16:44:35 -0500

    added turn function

--- drivetrain.cpp
+++ drivetrain.cpp
@@ -7,3 +7,7 @@ using namespace std;
 void move_forward(int distance) {
   cout << "moved robot " << distance << " units.\n";
 }
+
+void turn(double degrees) {
+  cout << "turned robot " << degrees << " degrees.\n";
+}
--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int main() {
   cout << "Started robot!\n";
   move_forward(10);
+  turn(31.7);
 
   return 0;
 }
