fdfe9886307be8d9af8c9ad41f524471aec50615
SomewhatMay <umayeer.ahsan@hotmail.com>
2024-12-17 16:01:17 -0500

    degrees to radians

--- drivetrain.cpp
+++ drivetrain.cpp
@@ -8,6 +8,6 @@ void move_forward(int distance) {
   cout << "moved robot " << distance << " units.\n";
 }
 
-void turn(double degrees) {
-  cout << "turned robot " << degrees << " degrees.\n";
+void turn(double radians) {
+  cout << "turned robot " << radians << " radians.\n";
 }
